0d2c5bac51bf019cf595f318c637a7374d9cb4cb
jiangzenghui005@163.com <jiangzenghui005@163.com>
2018-07-23 11:55:35 +0800

    Initial Commit

---
+++ TestSignalMethod/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestSignalMethod
+//
+//  Created by ys on 2018/7/23.
+//  Copyright © 2018年 ys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
